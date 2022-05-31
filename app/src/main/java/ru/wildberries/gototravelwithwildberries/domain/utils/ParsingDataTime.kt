8b732fd20c59604743d73ba2171cb154fe3bdaea
mcmouse88 <mcmouse88@icloud.com>
2022-06-01 00:12:17 +0200

    create data formatter utils

---
+++ app/src/main/java/ru/wildberries/gototravelwithwildberries/domain/utils/ParsingDataTime.kt
@@ -0,0 +1,56 @@
+package ru.wildberries.gototravelwithwildberries.domain.utils
+
+import java.time.DayOfWeek
+import java.time.LocalDateTime
+import java.time.Month
+import java.time.format.DateTimeFormatter
+
+fun parsingDate(date: String): String {
+    val localDateTime = parsingDateTime(date)
+    val outFormatter = DateTimeFormatter.ofPattern("dd")
+    return "${outFormatter.format(localDateTime)} ${getMonth(localDateTime)}, ${getDayOfWeek(localDateTime)}"
+}
+
+fun parsingTime(date: String): String {
+    val localTime = parsingDateTime(date)
+    val outFormatter = DateTimeFormatter.ofPattern("HH:mm")
+    return outFormatter.format(localTime)
+}
+
+private fun parsingDateTime(date: String): LocalDateTime {
+    val inFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    return LocalDateTime.parse(date, inFormatter)
+}
+
+private fun getDayOfWeek(localDateTime: LocalDateTime): String {
+    // TODO throw exception
+    return when(localDateTime.dayOfWeek) {
+        DayOfWeek.MONDAY -> "Пн"
+        DayOfWeek.TUESDAY -> "Вт"
+        DayOfWeek.WEDNESDAY -> "Ср"
+        DayOfWeek.THURSDAY -> "Чт"
+        DayOfWeek.FRIDAY -> "Пт"
+        DayOfWeek.SATURDAY -> "Сб"
+        DayOfWeek.SUNDAY -> "Вс"
+        null -> ""
+    }
+}
+
+private fun getMonth(localDateTime: LocalDateTime): String {
+    // TODO throw exception
+    return when(localDateTime.month) {
+        Month.JANUARY -> "Января"
+        Month.FEBRUARY -> "Февраля"
+        Month.MARCH -> "Марта"
+        Month.APRIL -> "Апреля"
+        Month.MAY -> "Мая"
+        Month.JUNE -> "Июня"
+        Month.JULY -> "Июля"
+        Month.AUGUST -> "Августа"
+        Month.SEPTEMBER -> "Сентября"
+        Month.OCTOBER -> "Октября"
+        Month.NOVEMBER -> "Ноября"
+        Month.DECEMBER -> "Декабря"
+        null -> ""
+    }
+}
--- app/src/main/java/ru/wildberries/gototravelwithwildberries/presentation/screens/MainScreen.kt
+++ app/src/main/java/ru/wildberries/gototravelwithwildberries/presentation/screens/MainScreen.kt
@@ -1,24 +1,83 @@
 package ru.wildberries.gototravelwithwildberries.presentation.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import ru.wildberries.gototravelwithwildberries.data.navigation.Screens
+import ru.wildberries.gototravelwithwildberries.domain.pojo.Data
 import ru.wildberries.gototravelwithwildberries.presentation.viewmodels.MainViewModel
+import ru.wildberries.gototravelwithwildberries.domain.utils.parsingDate
+import ru.wildberries.gototravelwithwildberries.domain.utils.parsingTime
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MainScreen(navController: NavHostController, viewModel: MainViewModel) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    val allData = viewModel.allData.observeAsState(listOf()).value
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.padding(20.dp)) {
+            items(allData) {item ->
+                TicketItem(item = item, navController = navController)
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun TicketItem(item: Data, navController: NavHostController) {
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable {
+                navController.navigate(Screens.Detail.route)
+            }
     ) {
-        Text(text = "This is Main Screen", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = parsingDate(item.startDate),
+                    fontSize = 14.sp
+                )
+
+                Text(
+                    text = parsingDate(item.endDate),
+                    fontSize = 14.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(
+                    text = parsingTime(item.startDate),
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = parsingTime(item.startDate),
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
 }
